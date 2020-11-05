4a345b9b525f70f2cc64c5c2c534bb10910ffa5b
ihsavru <vurvashi11@gmail.com>
2020-11-05 17:34:12 +0530

    minor fix

--- _data/blog/2020-02-20-fillerama.md
+++ _data/blog/2020-02-20-fillerama.md
@@ -4,6 +4,7 @@ path: /fillerama
 date: 2020-02-20T14:59:36.571Z
 title: Dummy Content from Fillerama
 thumbnail: /assets/image-5.jpg
+metaDescription: x
 ---
 # No, I'll fix it. I'm good at fixing rot. Call me the Rotmeister. No, I'm the Doctor. Don't call me the Rotmeister.
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <p>&copy; {new Date().getFullYear()} &bull; Crafted with <span role="img" aria-label="love">❤️</span> by The Code Dose</p>
       </footer>
     </div>
   )
