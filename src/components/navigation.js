2bb82cbe3d89cc7bc6b58b2badfc23a3561ab2c6
ihsavru <vurvashi11@gmail.com>
2023-01-04 19:30:41 +0900

    Add mobile menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 
-export default () => (
-  <nav className="site-header__navigation">
-    <Link to="/">
-      Home
-    </Link>
-    <Link to="/blog">
-      Blog
-    </Link>
-    <Link to="/cs-illustrated">
-      CS Illustrated
-    </Link>
-    <a
-      href="https://www.redbubble.com/people/thecodedose/shop"
-      target="__blank"
-    >
-      Shop
-    </a>
-  </nav>
-);
+export default () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return(
+    <>
+    <nav className="site-header__navigation--desktop">
+      <Link to="/">
+        Home
+      </Link>
+      <Link to="/blog">
+        Blog
+      </Link>
+      <Link to="/cs-illustrated">
+        CS Illustrated
+      </Link>
+      <a
+        href="https://www.redbubble.com/people/thecodedose/shop"
+        target="__blank"
+      >
+        Shop
+      </a>
+    </nav>
+    <div className="site-header__navigation--menu-icon" onClick={() => setOpenMenu(!openMenu)}>Menu</div>
+    <nav className="site-header__navigation--mobile">
+      {openMenu && (
+        <div>
+          <Link to="/">
+            Home
+          </Link>
+          <Link to="/blog">
+            Blog
+          </Link>
+          <Link to="/cs-illustrated">
+            CS Illustrated
+          </Link>
+          <a
+            href="https://www.redbubble.com/people/thecodedose/shop"
+            target="__blank"
+          >
+            Shop
+          </a>
+        </div>
+      )}
+    </nav>
+    </>
+  )}
