1956f55822f68f4bbaa80eeb8b6a852c433ab18a
ihsavru <vurvashi11@gmail.com>
2023-04-25 17:22:04 +0530

    Add link in mobile menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,58 +1,47 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
+import React, { useState } from "react";
+import { Link } from "gatsby";
 
 export default () => {
   const [openMenu, setOpenMenu] = useState(false);
 
-  return(
+  return (
     <>
-    <nav className="site-header__navigation--desktop">
-      <Link to="/">
-        Home
-      </Link>
-      <Link to="/blog">
-        Blog
-      </Link>
-      <Link to="/travel">
-        Travel
-      </Link>
-      <Link to="/study-plans">
-        Study Plans
-      </Link>
-      <Link to="/cs-illustrated">
-        CS Illustrated
-      </Link>
-      <a
-        href="https://www.redbubble.com/people/thecodedose/shop"
-        target="__blank"
+      <nav className="site-header__navigation--desktop">
+        <Link to="/">Home</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/travel">Travel</Link>
+        <Link to="/study-plans">Study Plans</Link>
+        <Link to="/cs-illustrated">CS Illustrated</Link>
+        <a
+          href="https://www.redbubble.com/people/thecodedose/shop"
+          target="__blank"
+        >
+          Shop
+        </a>
+      </nav>
+      <div
+        className="site-header__navigation--menu-icon"
+        onClick={() => setOpenMenu(!openMenu)}
       >
-        Shop
-      </a>
-    </nav>
-    <div className="site-header__navigation--menu-icon" onClick={() => setOpenMenu(!openMenu)}>Menu</div>
-    <nav className="site-header__navigation--mobile">
-      {openMenu && (
-        <div>
-          <Link to="/">
-            Home
-          </Link>
-          <Link to="/blog">
-            Blog
-          </Link>
-          <Link to="/travel">
-            Travel
-          </Link>
-          <Link to="/cs-illustrated">
-            CS Illustrated
-          </Link>
-          <a
-            href="https://www.redbubble.com/people/thecodedose/shop"
-            target="__blank"
-          >
-            Shop
-          </a>
-        </div>
-      )}
-    </nav>
+        Menu
+      </div>
+      <nav className="site-header__navigation--mobile">
+        {openMenu && (
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/travel">Travel</Link>
+            <Link to="/study-plans">Study Plans</Link>
+            <Link to="/cs-illustrated">CS Illustrated</Link>
+            <a
+              href="https://www.redbubble.com/people/thecodedose/shop"
+              target="__blank"
+            >
+              Shop
+            </a>
+          </div>
+        )}
+      </nav>
     </>
-  )}
+  );
+};
