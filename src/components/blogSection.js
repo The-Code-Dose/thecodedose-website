590e0a3b08c35205aa0fce880e148a201b3df84e
ihsavru <vurvashi11@gmail.com>
2023-05-20 16:53:42 +0530

    Fix styles

--- src/components/blogSection.js
+++ src/components/blogSection.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import PostLink from './postLink';
-import Button from './button';
-import './blogSection.scss';
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import PostLink from "./postLink";
+import Button from "./button";
+import "./blogSection.scss";
 
 export default () => {
   const data = useStaticQuery(
@@ -16,7 +16,9 @@ export default () => {
         latestBlogPosts: allMarkdownRemark(
           limit: 4
           sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { draft: { eq: false }, featured: { eq: true } } }
+          filter: {
+            frontmatter: { draft: { eq: false }, featured: { eq: true } }
+          }
         ) {
           edges {
             node {
@@ -33,43 +35,46 @@ export default () => {
           }
         }
       }
-    `,
+    `
   );
 
   const {
     latestBlogPosts: { edges },
   } = data;
 
-  const posts = edges
-    .filter((edge) => !!edge.node.frontmatter.date);
+  const posts = edges.filter((edge) => !!edge.node.frontmatter.date);
 
   const featuredPost = posts.shift();
 
-  const LatestPosts = posts
-    .map((edge) => <PostLink key={edge.node.id} post={edge.node} />);
+  const LatestPosts = posts.map((edge) => (
+    <PostLink key={edge.node.id} post={edge.node} />
+  ));
 
   return (
     <div className="blog-section" id="blog">
       <div className="blog-section__heading">
-        <h1>
-          Read our blog
-        </h1>
+        <h1>Read our blog</h1>
       </div>
       <div className="blog-section__container">
         <div className="blog-section__featured">
           <h2 className="blog-section__featured-title">
             {featuredPost.node.frontmatter.title}
           </h2>
-          <img src={featuredPost.node.frontmatter.thumbnail} className="blog-section__featured-thumbnail" />
-          <p>
-            {featuredPost.node.excerpt}
-          </p>
-          <Button text="Read more" color="yellow" link to={featuredPost.node.frontmatter.path} />
+          <img
+            src={featuredPost.node.frontmatter.thumbnail}
+            className="blog-section__featured-thumbnail"
+          />
+          <div className="post-meta">{featuredPost.node.frontmatter.date}</div>
+          <p>{featuredPost.node.excerpt}</p>
+          <Button
+            text="Read more"
+            color="yellow"
+            link
+            to={featuredPost.node.frontmatter.path}
+          />
         </div>
-        <div className="blog-section__list">
-          {LatestPosts}
-        </div>
+        <div className="blog-section__list">{LatestPosts}</div>
       </div>
     </div>
-  )
-}
+  );
+};
