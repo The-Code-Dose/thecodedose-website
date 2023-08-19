a4e6ee4f8e15f6c71080c058679c6a2ae2c4d075
ihsavru <vurvashi11@gmail.com>
2023-08-19 14:56:13 +0530

    Add colors in tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    colors: {
+      purple: '#7562ab',
+      black: '#2a2a2a',
+      white: '#ffffff',
+      yellow: '#fbe20c',
+      grey: '#d4d4d4',
+      pink: '#ff84ca',
+      blue: '#9FD7FA',
+      red: '#F15555',
+      green: '#88fe77',
+    },
     extend: {},
   },
   plugins: [],
