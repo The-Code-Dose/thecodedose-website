27f275f2e192b33e5751068158ad82a1d19cde10
Vinathi <you@example.com>
2023-09-03 17:34:27 +0530

    Replaced current menu with Headless UI Menu component

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,60 +1,79 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
-import { Bars3Icon } from '@heroicons/react/24/outline';
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Menu, Transition } from "@headlessui/react";
 
 const links = [
   {
-    label: 'Home',
-    path: '/',
+    label: "Home",
+    path: "/",
   },
   {
-    label: 'Blog',
-    path: '/blog',
+    label: "Blog",
+    path: "/blog",
   },
   // {
   //   label: 'Resources',
   //   path: '/resources',
   // },
   {
-    label: 'Study Plans',
-    path: '/study-plans',
+    label: "Study Plans",
+    path: "/study-plans",
   },
   {
-    label: 'Shop',
-    path: 'https://www.redbubble.com/people/thecodedose/shop',
+    label: "Shop",
+    path: "https://www.redbubble.com/people/thecodedose/shop",
   },
 ];
 
 export default function () {
   const [openMenu, setOpenMenu] = useState(false);
-  const currentPath = typeof window !== "undefined" ? window.location.pathname : ""
+  const currentPath =
+    typeof window !== "undefined" ? window.location.pathname : "";
 
   const linkElements = (
-    <>{links.map(({ label, path }) => (
-      <Link className={`font-semibold rounded-full transition-all text-xs px-5 py-2 border uppercase hover:no-underline ${((currentPath.includes(path) && path !== '/') || (currentPath === '/' && path === '/')) ? 'bg-pink drop-shadow-solid border border-black hover:drop-shadow-solid-extend text-black' : 'text-white border-purple hover:border-white'}`} to={path}>
-        {label}
-      </Link>
-    ))}
+    <>
+      {links.map(({ label, path }) => (
+        <Menu.Item>
+          {({ active }) => (
+            <Link
+              className={`menu-item font-semibold rounded-full transition-all text-xs px-5 py-2 border uppercase hover:no-underline ${
+                (currentPath.includes(path) && path !== "/") ||
+                (currentPath === "/" && path === "/")
+                  ? "bg-pink drop-shadow-solid border border-black hover:drop-shadow-solid-extend text-black"
+                  : "text-white border-purple hover:border-white"
+              } ${active ? "active" : ""} p-0 m-0`}
+              to={path}
+            >
+              {label}
+            </Link>
+          )}
+        </Menu.Item>
+      ))}
     </>
   );
 
   return (
-    <>
-      <nav className="leading-normal hidden md:flex gap-4 lg:gap-6 justify-around items-center">
-        {linkElements}
-      </nav>
+    <div className="menu-container flex justify-between items-center w-full">
+      <Menu as="nav">
+        <Transition show={openMenu}>
+          <nav
+            className={`fixed top-20 flex flex-col left-0 bg-purple w-full rounded-xl px-6 py-5 border ${
+              openMenu ? "mt-3" : ""
+            }`}
+            style={{ maxWidth: "100%" }}
+          >
+            {linkElements}
+          </nav>
+        </Transition>
+      </Menu>
       <button
-        className="block md:hidden text-white uppercase font-bold outline-none"
+        className="text-white uppercase font-bold outline-none"
         onClick={() => setOpenMenu(!openMenu)}
         type="button"
       >
         <Bars3Icon className="w-6 h-6" />
       </button>
-      {openMenu && (
-        <nav className="fixed top-20 flex flex-col left-0 bg-purple w-full rounded-xl px-6 py-5 border">
-          {linkElements}
-        </nav>
-      )}
-    </>
+    </div>
   );
 }
