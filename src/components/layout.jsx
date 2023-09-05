c170e8da0c5cbe1fd99e7480ea2069a515d10f97
ihsavru <vurvashi11@gmail.com>
2023-09-06 04:44:42 +0530

    Update footer

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -21,51 +21,15 @@ const WrappedHeader = withPadding(
 );
 
 export default function ({ children }) {
-  const [subscribed, setSubscribed] = useState(false);
-  const [email, setEmail] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addToMailchimp(email)
-      .then(() => {
-        setSubscribed(true);
-        alert("Successfully subscribed!");
-      })
-      .catch(() => {
-        alert("Uh Oh! Something went wrong");
-      });
-  };
-
   return (
     <div className="w-screen">
       <WrappedHeader />
       {children}
       <footer className="bg-black flex flex-col md:flex-row justify-between gap-10 px-10 py-10">
-        <div className="bg-purple p-10 rounded-2xl border border-black drop-shadow-solid flex flex-col gap-5 justify-between">
+        <div className="p-10 rounded-2xl border border-black drop-shadow-solid flex flex-col gap-5 justify-between">
           <Link to="/">
             <img className="w-20" src={logo} alt="Logo" />
           </Link>
-          <p className="text-white">
-            Subscribe to receive latest updates right in your inbox!
-          </p>
-          <form
-            className="flex flex-col lg:flex-row justify-between"
-            onSubmit={handleSubmit}
-          >
-            <input
-              className="border w-full lg:w-1/2 border-black drop-shadow-solid my-2 px-4 py-2 rounded-full"
-              placeholder="Email Address"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <button
-              className="bg-pink text-black uppercase my-2 px-4 py-2 rounded-full border border-black drop-shadow-solid hover:scale-105 transition-all"
-              type="submit"
-              disabled={subscribed}
-            >
-              {subscribed ? "Subscribed" : "Subscribe!"}
-            </button>
-          </form>
         </div>
         <SocialLinks />
       </footer>
--- src/components/socialLinks.jsx
+++ src/components/socialLinks.jsx
@@ -3,6 +3,33 @@ import { useStaticQuery, graphql, Link } from 'gatsby';
 
 import './socialLinks.scss';
 
+const links = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Blog",
+    path: "/blog",
+  },
+  // {
+  //   label: 'Resources',
+  //   path: '/resources',
+  // },
+  {
+    label: "Study Plans",
+    path: "/study-plans",
+  },
+  {
+    label: "Shop",
+    path: "https://www.redbubble.com/people/thecodedose/shop",
+  },
+  {
+    label: "Apply for next React Cohort",
+    path: "https://forms.gle/KVFTHZYj3dXnanRV9",
+  },
+];
+
 export default function () {
   const data = useStaticQuery(graphql`
     query SocialQuery {
@@ -25,42 +52,46 @@ export default function () {
     site: {
       siteMetadata: {
         social: {
-          instagram, redbubble, twitter, github, discord, youtube,
+          instagram, twitter, github, discord, youtube,
         },
       },
     },
   } = data;
 
+  const social = [
+    {
+      label: "Instagram",
+      path: instagram,
+    },
+    {
+      label: "Discord",
+      path: discord,
+    },
+    {
+      label: "Twitter",
+      path: twitter,
+    },
+    {
+      label: "YouTube",
+      path: youtube,
+    },
+    {
+      label: "GitHub",
+      path: github,
+    },
+  ];
+
   return (
-    <div className="flex flex-col md:flex-row gap-5 w-full md:w-1/2 bg-blue  p-10 rounded-2xl border border-black drop-shadow-solid">
-      <div className="flex text-xs md:text-right flex-col justify-between  w-full md:w-1/2">
-        <Link className="text-black" to="/">Home</Link>
-        <Link className="text-black" to="/blog">Blog</Link>
-        <Link className="text-black" to="/travel">Travel</Link>
-        <Link className="text-black" to="/cs-illustrated">CS Illustrated</Link>
-        <Link className="text-black" to="https://forms.gle/KVFTHZYj3dXnanRV9">
-          Apply for next React Cohort
-        </Link>
-        <a className="text-black" href={redbubble} target="__blank">
-          Shop
-        </a>
+    <div className="flex flex-col md:flex-row gap-5 w-full md:w-1/2  p-10 rounded-2xl border border-black drop-shadow-solid">
+      <div className="flex text-xs md:text-right flex-col justify-between gap-5 w-full md:w-1/2 ">
+        {links.map(({ path, label }) => <Link className="text-white" to={path}>{label}</Link>)}
       </div>
-      <div className="flex flex-col justify-between text-xs md:text-right w-full md:w-1/2">
-        <a className="text-black" href={instagram} target="__blank">
-          Instagram
-        </a>
-        <a className="text-black" href={discord} target="__blank">
-          Discord
-        </a>
-        <a className="text-black" href={twitter} target="__blank">
-          Twitter
-        </a>
-        <a className="text-black" href={youtube} target="__blank">
-          YouTube
-        </a>
-        <a className="text-black" href={github} target="__blank">
-          Github
-        </a>
+      <div className="flex flex-col justify-between text-xs md:text-right gap-5  w-full md:w-1/2">
+        {social.map(({ path, label }) => (
+          <a className="text-white" href={path} target="__blank">
+            {label}
+          </a>
+        ))}
       </div>
     </div>
   );
