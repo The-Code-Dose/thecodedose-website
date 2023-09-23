fe67891df1d04430fd1e8b3ceb5f324bec3c063b
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-09-23 18:05:09 +0200

    facebook and linkedin share linkks

--- src/components/socialShare.jsx
+++ src/components/socialShare.jsx
@@ -5,13 +5,27 @@ function SocialShare({ title }) {
 
   return (
     <div>
-        <a
-          href={`https://twitter.com/share?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(title)}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
+      <a
+        href={`https://twitter.com/share?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(title)}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+      <a
+        href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Facebook
+      </a>
+      <a
+        href={`https://www.linkedin.com/shareArticle?url=${encodeURIComponent(shareUrl)}&title=${encodeURIComponent(title)}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
     </div>
   )
 }
