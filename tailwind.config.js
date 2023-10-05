7a75aae2b88e198ceca1c149e5a9a0b70be9e2bc
ihsavru <vurvashi11@gmail.com>
2023-10-05 16:14:52 +0530

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
